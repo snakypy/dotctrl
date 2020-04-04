6dc9c568166b89b1126a38e9f9009bc6d34de73d
William C. Canin <william.costa.canin@gmail.com>
2020-04-04 06:09:15 -0300

    :hammer: Formatting code.

--- dotctrl/dotctrl.py
+++ dotctrl/dotctrl.py
@@ -13,7 +13,7 @@ from snakypy import FG, printer
 from snakypy.ansi import NONE
 from dotctrl import __pkginfo__, __version__, config, utils
 
-
+DANGER = f"{FG.YELLOW}DANGER!{NONE}"
 menu_opts = f"""
 {__pkginfo__['name']} version: {FG.CYAN}{__version__}{NONE}
 
@@ -35,32 +35,27 @@ USAGE:
 
 ARGUMENTS:
     {FG.CYAN}init{NONE} ----------- Creates the dotfiles repository.
-    {FG.CYAN}pull{NONE} ----------- Pulls the dotfiles from the source location
-                     on the machine to the repository.
-    {FG.CYAN}link{NONE} ----------- Links the repository's dotfiles to the source
-                     location on the machine.
-    {FG.CYAN}check{NONE} ---------- Checks whether the dotfiles in the
-                     repository are linked to the place of origin or not.
-    {FG.CYAN}list{NONE} ----------- Lists all files present in the repository
-                     and in the configuration.
-    {FG.CYAN}unlink{NONE} --------- Unlink the dotfiles from the repository with
-                     the original location on the machine.
-    {FG.CYAN}config{NONE} --------- Open or view settings.
-    {FG.CYAN}restore{NONE} -------- Moves all dotfiles from the repository to the
-                     default source location.
-    {FG.CYAN}remove{NONE} --------- {FG.YELLOW}DANGER!{NONE} Removes the elements in the
-                     repository and symbolic link in the source location.
+    {FG.CYAN}pull{NONE} ----------- Pulls the dotfiles from the source location on the machine to the repository.
+    {FG.CYAN}link{NONE} ----------- Links the repository's dotfiles to the source location on the machine.
+    {FG.CYAN}check{NONE} ---------- Checks whether the dotfiles in the repository are linked to the place of
+                     origin or not.
+    {FG.CYAN}list{NONE} ----------- Lists all files present in the repository and in the configuration.
+    {FG.CYAN}unlink{NONE} --------- Unlink the dotfiles from the repository with the original location on the machine.
+    {FG.CYAN}config{NONE} --------- Handles the configuration file.
+    {FG.CYAN}restore{NONE} -------- Moves all dotfiles from the repository to the default source location.
+    {FG.CYAN}remove{NONE} --------- {DANGER} Removes the elements in the repository and symbolic link in the source
+                     location.
 
 OPTIONS:
-    {FG.BLUE}--element=<object>{NONE} ---- Receive an object where, have the absolute
-                            path to a file or folder, always from the HOME directory.
-    {FG.BLUE}--open{NONE} ---------------- Open the configuration file in edit mode and
-                            perform the automatic update when you exit.
+    {FG.BLUE}--element=<object>{NONE} ---- Receive an object where, have the absolute path to a file or folder,
+                            always from the HOME directory.
+    {FG.BLUE}--open{NONE} ---------------- Open the configuration file in edit mode and perform the automatic update
+                            when you exit.
     {FG.BLUE}--view{NONE} ---------------- View the configuration file on the terminal.
     {FG.BLUE}--git{NONE} ----------------- Create a Git repository.
-    {FG.BLUE}--force{NONE} --------------- Complete the command regardless of whether or
-                            not files exist.
+    {FG.BLUE}--force{NONE} --------------- Complete the command regardless of whether or not files exist.
     {FG.BLUE}--all{NONE} ----------------- Perform a mass action.
+    {FG.BLUE}--noconfirm{NONE} ----------- Perform an action without calling a confirmation.
     {FG.BLUE}--version{NONE} ------------- Show version.
     {FG.BLUE}--credits{NONE} ------------- Show credits.
     {FG.BLUE}--help{NONE} ---------------- Show this screen.
