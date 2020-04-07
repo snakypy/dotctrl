b4f98534265c2a5a30a5a17e7e70cbc9a3a03871
William C. Canin <william.costa.canin@gmail.com>
2020-04-07 01:29:10 -0300

    :rotating_light: Reformatting test code

--- tests/test_dotctrl.py
+++ tests/test_dotctrl.py
@@ -1,16 +1,23 @@
 import pytest
 import snakypy
-from os.path import exists, islink, join
-from dotctrl import decorators
-from dotctrl.dotctrl import Dotctrl
+from os.path import exists, islink, join, isfile
+from dotctrl.console.utils.decorators import assign_cli
+from dotctrl.config.base import Base
+from dotctrl.console.init import InitCommand
+from dotctrl.console.credits import CreditsCommand
+from dotctrl.console.pull import PullCommand
+from dotctrl.console.link import LinkCommand
+from dotctrl.console.unlink import UnlinkCommand
+from dotctrl.console.restore import RestoreCommand
+from dotctrl.console.check import CheckCommand
+from dotctrl.console.utils import arguments
 
 
 @pytest.fixture
 def base(tmpdir):
     home = tmpdir.mkdir("home")
     root = tmpdir.mkdir("home/Dotfiles")
-    inst = Dotctrl(root, home)
-    for item in inst.text_editors:
+    for item in Base(root, home).editors_config:
         path_split = item.split("/")[:-1]
         path_str = "/".join(path_split)
         path = join(home, path_str)
@@ -18,15 +25,13 @@ def base(tmpdir):
     return {"root": root, "home": home}
 
 
-def class_dotctrl(base):
-    inst = Dotctrl(base["root"], base["home"])
+def class_base(base):
+    inst = Base(base["root"], base["home"])
     return inst
 
 
 def dotctrl_init(base):
-    return class_dotctrl(base).init_command(
-        class_dotctrl(base).arguments(argv=["init"])
-    )
+    InitCommand(base["root"], base["home"]).main(arguments(argv=["init"]))
 
 
 def dotfiles_tests(base, create=True):
@@ -36,18 +41,18 @@ def dotfiles_tests(base, create=True):
             snakypy.file.create("dotctrl tests", join(base["home"], item), force=True)
             if not exists(join(base["home"], item)):
                 assert False
-        for item in class_dotctrl(base).text_editors:
-            snakypy.file.create("dotctrl tests", join(base["home"], item))
+        for item in Base(base["root"], base["home"]).editors_config:
+            snakypy.file.create("dotctrl tests", join(base["home"], item), force=True)
     return files_text
 
 
 def update_config_elements(base, *files):
-    parsed = class_dotctrl(base).parsed
+    parsed = class_base(base).parsed
     parsed["dotctrl"]["elements"] = [*files]
     parsed["dotctrl"]["smart"]["rc"]["enable"] = True
     parsed["dotctrl"]["smart"]["text_editors"]["enable"] = True
-    snakypy.json.update(class_dotctrl(base).config, parsed)
-    new_parsed = class_dotctrl(base).parsed
+    snakypy.json.update(class_base(base).config_path, parsed)
+    new_parsed = class_base(base).parsed
     assert new_parsed["dotctrl"]["elements"] == [*files]
     assert new_parsed["dotctrl"]["smart"]["rc"]["enable"] is True
     assert new_parsed["dotctrl"]["smart"]["text_editors"]["enable"] is True
@@ -56,76 +61,197 @@ def update_config_elements(base, *files):
 def test_cli(base):
     dotfiles_tests(base)
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["init"]), "init")
+    @assign_cli(arguments(argv=["init"]), "init")
     def init():
         dotctrl_init(base)
-        if not exists(class_dotctrl(base).config):
+        if not exists(class_base(base).config_path):
             assert False
-        if not exists(class_dotctrl(base).repo):
+        if not exists(class_base(base).repo_path):
             assert False
 
-    @decorators.assign_cli(
-        class_dotctrl(base).arguments(argv=["--credits"]), "--credits"
-    )
+    @assign_cli(arguments(argv=["--credits"]), "--credits")
     def credence():
-        class_dotctrl(base).credence()
+        CreditsCommand().main()
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["pull"]), "pull")
+    @assign_cli(arguments(argv=["pull"]), "pull")
     def pull():
         update_config_elements(base, ".config/foo.txt", ".config/bar.txt")
-        class_dotctrl(base).pull_command(class_dotctrl(base).arguments(argv=["pull"]))
+        PullCommand(base["root"], base["home"]).main(arguments(argv=["pull"]))
         for item in dotfiles_tests(base, create=False):
-            if not exists(join(class_dotctrl(base).repo, item)):
+            if not exists(join(class_base(base).repo_path, item)):
                 assert False
-        for item in class_dotctrl(base).text_editors:
-            if not exists(join(class_dotctrl(base).repo, item)):
+        for item in class_base(base).editors_config:
+            if not exists(join(class_base(base).repo_path, item)):
                 assert False
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["check"]), "check")
+    @assign_cli(arguments(argv=["check"]), "check")
     def check():
-        class_dotctrl(base).check_command()
+        CheckCommand(base["root"], base["home"]).main()
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["link"]), "link")
+    @assign_cli(arguments(argv=["link"]), "link")
     def link():
-        class_dotctrl(base).link_command(class_dotctrl(base).arguments(argv=["link"]))
+        LinkCommand(base["root"], base["home"]).main(
+            arguments(argv=["link", "--force"])
+        )
         for item in dotfiles_tests(base, create=False):
-            if not islink(join(class_dotctrl(base).HOME, item)):
+            if not islink(join(class_base(base).HOME, item)):
                 assert False
-        for item in class_dotctrl(base).text_editors:
-            if not islink(join(class_dotctrl(base).HOME, item)):
+        for item in class_base(base).editors_config:
+            if not islink(join(class_base(base).HOME, item)):
                 assert False
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["unlink"]), "unlink")
+    @assign_cli(arguments(argv=["unlink"]), "unlink")
     def unlink():
-        class_dotctrl(base).unlink_command(
-            class_dotctrl(base).arguments(argv=["unlink"])
-        )
+        UnlinkCommand(base["root"], base["home"]).main(arguments(argv=["unlink"]))
         for item in dotfiles_tests(base, create=False):
-            if islink(join(class_dotctrl(base).HOME, item)):
+            if islink(join(class_base(base).HOME, item)):
                 assert False
-        for item in class_dotctrl(base).text_editors:
-            if islink(join(class_dotctrl(base).HOME, item)):
+        for item in class_base(base).editors_config:
+            if islink(join(class_base(base).HOME, item)):
                 assert False
 
-    @decorators.assign_cli(class_dotctrl(base).arguments(argv=["restore"]), "restore")
+    @assign_cli(arguments(argv=["restore"]), "restore")
     def restore():
-        class_dotctrl(base).restore_command(
-            class_dotctrl(base).arguments(argv=["restore"])
+        RestoreCommand(base["root"], base["home"]).main(
+            arguments(
+                argv=[
+                    "restore",
+                    f"--element={dotfiles_tests(base, create=False)[0]}",
+                    "--force",
+                ]
+            )
         )
-        for item in dotfiles_tests(base, create=False):
-            if exists(join(class_dotctrl(base).repo, item)):
-                assert False
-        for item in class_dotctrl(base).text_editors:
-            if exists(join(class_dotctrl(base).repo, item)):
-                assert False
-        for item in class_dotctrl(base).text_editors:
-            if islink(join(class_dotctrl(base).HOME, item)):
-                assert False
+        f = join(class_base(base).HOME, dotfiles_tests(base, create=False)[0])
+        if not isfile(f):
+            assert False
+
+        # for item in dotfiles_tests(base, create=False):
+        #     if exists(join(class_base(base).repo_path, item)):
+        #         assert False
+        # for item in class_base(base).editors_config:
+        #     if exists(join(class_base(base).repo_path, item)):
+        #         assert False
+        # for item in class_base(base).editors_config:
+        #     if islink(join(class_base(base).HOME, item)):
+        #         assert False
 
     init()
-    credence()
+    # credence()
     pull()
     link()
     unlink()
     check()
     restore()
+
+
+# def class_dotctrl(base):
+#     inst = Dotctrl(base["root"], base["home"])
+#     return inst
+
+
+# def dotctrl_init(base):
+#   return class_dotctrl(base).init_command(
+#       class_dotctrl(base).arguments(argv=["init"])
+#   )
+
+#
+# def dotfiles_tests(base, create=True):
+#     files_text = [".dotctrlrc", ".config/foo.txt"]
+#     if create:
+#         for item in files_text:
+#             snakypy.file.create("dotctrl tests", join(base["home"], item), force=True)
+#             if not exists(join(base["home"], item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             snakypy.file.create("dotctrl tests", join(base["home"], item))
+#     return files_text
+#
+#
+# def update_config_elements(base, *files):
+#     parsed = class_dotctrl(base).parsed
+#     parsed["dotctrl"]["elements"] = [*files]
+#     parsed["dotctrl"]["smart"]["rc"]["enable"] = True
+#     parsed["dotctrl"]["smart"]["text_editors"]["enable"] = True
+#     snakypy.json.update(class_dotctrl(base).config, parsed)
+#     new_parsed = class_dotctrl(base).parsed
+#     assert new_parsed["dotctrl"]["elements"] == [*files]
+#     assert new_parsed["dotctrl"]["smart"]["rc"]["enable"] is True
+#     assert new_parsed["dotctrl"]["smart"]["text_editors"]["enable"] is True
+#
+#
+# def test_cli(base):
+#     dotfiles_tests(base)
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["init"]), "init")
+#     def init():
+#         dotctrl_init(base)
+#         if not exists(class_dotctrl(base).config):
+#             assert False
+#         if not exists(class_dotctrl(base).repo):
+#             assert False
+#
+#     @assign_cli(
+#         class_dotctrl(base).arguments(argv=["--credits"]), "--credits"
+#     )
+#     def credence():
+#         class_dotctrl(base).credence()
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["pull"]), "pull")
+#     def pull():
+#         update_config_elements(base, ".config/foo.txt", ".config/bar.txt")
+#         class_dotctrl(base).pull_command(class_dotctrl(base).arguments(argv=["pull"]))
+#         for item in dotfiles_tests(base, create=False):
+#             if not exists(join(class_dotctrl(base).repo, item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             if not exists(join(class_dotctrl(base).repo, item)):
+#                 assert False
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["check"]), "check")
+#     def check():
+#         class_dotctrl(base).check_command()
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["link"]), "link")
+#     def link():
+#         class_dotctrl(base).link_command(class_dotctrl(base).arguments(argv=["link"]))
+#         for item in dotfiles_tests(base, create=False):
+#             if not islink(join(class_dotctrl(base).HOME, item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             if not islink(join(class_dotctrl(base).HOME, item)):
+#                 assert False
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["unlink"]), "unlink")
+#     def unlink():
+#         class_dotctrl(base).unlink_command(
+#             class_dotctrl(base).arguments(argv=["unlink"])
+#         )
+#         for item in dotfiles_tests(base, create=False):
+#             if islink(join(class_dotctrl(base).HOME, item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             if islink(join(class_dotctrl(base).HOME, item)):
+#                 assert False
+#
+#     @assign_cli(class_dotctrl(base).arguments(argv=["restore"]), "restore")
+#     def restore():
+#         class_dotctrl(base).restore_command(
+#             class_dotctrl(base).arguments(argv=["restore"])
+#         )
+#         for item in dotfiles_tests(base, create=False):
+#             if exists(join(class_dotctrl(base).repo, item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             if exists(join(class_dotctrl(base).repo, item)):
+#                 assert False
+#         for item in class_dotctrl(base).text_editors:
+#             if islink(join(class_dotctrl(base).HOME, item)):
+#                 assert False
+#
+#     init()
+#     credence()
+#     pull()
+#     link()
+#     unlink()
+#     check()
+#     restore()
