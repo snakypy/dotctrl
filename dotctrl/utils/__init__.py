a08280b042299dc05fd2862bcfde592a8dc17a01
William C. Canin <william.costa.canin@gmail.com>
2020-04-08 02:55:26 -0300

    Adding shorten_path function import

--- dotctrl/utils/__init__.py
+++ dotctrl/utils/__init__.py
@@ -1,7 +1,6 @@
-from . import decorators  # noqa
 from .process import git_init_command  # noqa
 from .checking import check_init  # noqa
-from .path import join_two, path_creation  # noqa
+from .path import join_two, path_creation, shorten_path  # noqa
 from .catch import listing_files, arguments  # noqa
 from .shift import (
     create_symlink,
