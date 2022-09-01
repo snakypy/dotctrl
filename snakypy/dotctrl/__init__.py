cd3d5edafdabde371fa389b97dd5c51bb4810b02
William C. Canin <william.costa.canin@gmail.com>
2022-09-01 04:46:12 -0300

    :bookmark: Update version beta

--- snakypy/dotctrl/__init__.py
+++ snakypy/dotctrl/__init__.py
@@ -21,7 +21,7 @@ from snakypy.helpers.files import eqversion
 
 __info__ = {
     "name": "Dotctrl",
-    "version": "1.5.1",
+    "version": "1.6.0b1",
     "description": "Dotctrl is a package for managing your dotfiles on Linux.",
     "pkg_name": "dotctrl",
     "executable": "dotctrl",
