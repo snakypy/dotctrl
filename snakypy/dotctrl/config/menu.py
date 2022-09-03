29d199953a5e55616a382343da52fe42288dac3c
William C. Canin <william.costa.canin@gmail.com>
2022-09-02 23:38:31 -0300

    :hammer: Changing flags name

--- snakypy/dotctrl/config/menu.py
+++ snakypy/dotctrl/config/menu.py
@@ -19,11 +19,12 @@ def menu():
         {__info__['executable']} init [--auto | --git]
         {__info__['executable']} pull [--e=<object> | --element=<object>] [--f | --force]
         {__info__['executable']} link [--e=<object> | --element=<object>] [--f | --force]
+        {__info__['executable']} copy [--e=<object> | --element=<object>] [--f | --force]
         {__info__['executable']} unlink [--e=<object> | --element=<object>] [--f | --force]
-        {__info__['executable']} restore [--e=<object> | --element=<object>] [--f | --force] [--keep-record]
-        {__info__['executable']} repo (--check | --reg | --info)
+        {__info__['executable']} restore [--e=<object> | --element=<object>] [--f | --force]
+        {__info__['executable']} repo (--check | --list | --info)
         {__info__['executable']} find (--name=<object>)
-        {__info__['executable']} config (--open | --view)
+        {__info__['executable']} config (--open | --view | --autoclean)
         {__info__['executable']} --help
         {__info__['executable']} --version
         {__info__['executable']} --credits
@@ -52,12 +53,12 @@ def menu():
                                     directory. On Linux it will create in "/home", on macOS in "/Users".
                                     You must have super user permission.
         {FG().BLUE}--info{NONE} ---------------------- Shows detailed information for the Do Ctrl repository.
-        {FG().BLUE}--reg{NONE} ----------------------- List all files and folders in the {__info__["name"]} repository.
+        {FG().BLUE}--list{NONE} ----------------------- List all files and folders in the {__info__["name"]} repository.
         {FG().BLUE}--name <object>{NONE} ------------- Receives the name of an object, it can be a folder or a file.
         {FG().BLUE}--f | --force{NONE} --------------- Complete the command regardless of whether or not files exist.
                                     ATTENTION! Using this option will replace existing files.
-        {FG().BLUE}--keep-record{NONE} --------------- Keep object in the {__info__["name"]} registry
-                                    ({__info__["config"]}).
+        {FG().BLUE}--autoclean{NONE} ----------------- Clear the {__info__["name"]} registry, if there is no element in the
+                                    repository.
         {FG().BLUE}--version{NONE} ------------------- Show version.
         {FG().BLUE}--credits{NONE} ------------------- Show credits.
         {FG().BLUE}--help{NONE} ---------------------- Show this screen.
@@ -75,11 +76,12 @@ def menu():
         {__info__['executable']} init [--auto | --git]
         {__info__['executable']} pull [--e=<object> | --element=<object>] [--f | --force]
         {__info__['executable']} link [--e=<object> | --element=<object>] [--f | --force]
+        {__info__['executable']} copy [--e=<object> | --element=<object>] [--f | --force]
         {__info__['executable']} unlink [--e=<object> | --element=<object>] [--f | --force]
-        {__info__['executable']} restore [--e=<object> | --element=<object>] [--f | --force] [--keep-record]
-        {__info__['executable']} repo (--check | --reg | --info)
+        {__info__['executable']} restore [--e=<object> | --element=<object>] [--f | --force]
+        {__info__['executable']} repo (--check | --list | --info)
         {__info__['executable']} find (--name=<object>)
-        {__info__['executable']} config (--open | --view)
+        {__info__['executable']} config (--open | --view | --autoclean)
         {__info__['executable']} --help
         {__info__['executable']} --version
         {__info__['executable']} --credits
@@ -88,32 +90,32 @@ def menu():
         {FG().CYAN}init{NONE} ----------- Creates the dotfiles repository.
         {FG().CYAN}find{NONE} ----------- Find some object in the Dotctrl repository.
         {FG().CYAN}pull{NONE} ----------- Pulls the dotfiles from the source location on the
-                        machine to the repository.
+                         machine to the repository.
         {FG().CYAN}link{NONE} ----------- Links the repository's dotfiles to the source location
-                        on the machine.
+                         on the machine.
         {FG().CYAN}repo{NONE} ----------- Show {__info__["name"]} repository information.
         {FG().CYAN}unlink{NONE} --------- Unlink the dotfiles from the repository with the original location
-                        on the machine.
+                         on the machine.
         {FG().CYAN}config{NONE} --------- Handles the configuration file.
         {FG().CYAN}restore{NONE} -------- Moves all dotfiles from the repository to the default source location.
 
     OPTIONS:
         {FG().BLUE}--check{NONE} --------------------- Checks whether the dotfiles in the repository are linked to
-                                    the place of origin or not.
+                                      the place of origin or not.
         {FG().BLUE}--e | --element <object>{NONE} ---- Receives an object without its absolute path, just the relative one.
         {FG().BLUE}--open{NONE} ---------------------- Open a file in edit mode.
         {FG().BLUE}--view{NONE} ---------------------- View the contents of a file in the terminal.
         {FG().BLUE}--git{NONE} ----------------------- Create a Git repository.
         {FG().BLUE}--auto{NONE} ---------------------- Creates the {__info__["name"]} repository automatically in the users
-                                    directory. On Linux it will create in "/home", on macOS in "/Users".
-                                    You must have super user permission.
+                                      directory. On Linux it will create in "/home", on macOS in "/Users".
+                                      You must have super user permission.
         {FG().BLUE}--info{NONE} ---------------------- Shows detailed information for the Do Ctrl repository.
-        {FG().BLUE}--reg{NONE} ----------------------- List all files and folders in the {__info__["name"]} repository.
+        {FG().BLUE}--list{NONE} ---------------------- List all files and folders in the {__info__["name"]} repository.
         {FG().BLUE}--name <object>{NONE} ------------- Receives the name of an object, it can be a folder or a file.
         {FG().BLUE}--f | --force{NONE} --------------- Complete the command regardless of whether or not files exist.
-                                    ATTENTION! Using this option will replace existing files.
-        {FG().BLUE}--keep-record{NONE} --------------- Keep object in the {__info__["name"]} registry
-                                    ({__info__["config"]}).
+                                      ATTENTION! Using this option will replace existing files.
+        {FG().BLUE}--autoclean{NONE} ----------------- Clear the {__info__["name"]} registry, if there is no element in the
+                                      repository.
         {FG().BLUE}--version{NONE} ------------------- Show version.
         {FG().BLUE}--credits{NONE} ------------------- Show credits.
         {FG().BLUE}--help{NONE} ---------------------- Show this screen.
