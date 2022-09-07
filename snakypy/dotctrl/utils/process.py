7d730237ddba0ce6eacba3aec061a72db10897a2
William C. Canin <william.costa.canin@gmail.com>
2022-09-07 12:05:00 -0300

    :hammer: :bug: Adding paramete in function 'git_init_command'

--- snakypy/dotctrl/utils/process.py
+++ snakypy/dotctrl/utils/process.py
@@ -1,5 +1,5 @@
 from getpass import getpass
-from os.path import isdir
+from os.path import isdir, join
 from shutil import which
 from subprocess import PIPE, Popen, call, run
 
@@ -7,10 +7,10 @@ from snakypy.helpers.ansi import FG
 from snakypy.helpers.console import printer
 
 
-def git_init_command() -> None:
+def git_init_command(directory: str) -> None:
     """Function to start a Git repository in the Dotctrl repository."""
-    if which("git") and not isdir(".git"):
-        call(["git", "init"], stdout=PIPE)
+    if which("git") and not isdir(join(directory, ".git")):
+        call(["git", "init", directory], stdout=PIPE)
 
 
 def super_command(commands: list, msg_err: str, msg_interrupt: str) -> bool:
