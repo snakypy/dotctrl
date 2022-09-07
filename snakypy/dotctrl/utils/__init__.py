0086683570d19942143395a093bb1d17aaaf641c
William C. Canin <william.costa.canin@gmail.com>
2022-09-07 11:40:55 -0300

    :hammer: Code improvements

--- snakypy/dotctrl/utils/__init__.py
+++ snakypy/dotctrl/utils/__init__.py
@@ -1,9 +1,9 @@
 from .catch import arguments, listing_files  # noqa
 from .dialog import pick  # noqa
-from .checking import is_repo_symbolic_link, lang_sys  # noqa
+from .checking import is_repo_symbolic_link, get_key  # noqa
 from .path import join_two, path_creation, shorten_path  # noqa
 from .process import git_init_command  # noqa
-from .shift import (  # noqa
+from .modifiers import (  # noqa
     add_element_config,
     create_symlink,
     remove_objects,
