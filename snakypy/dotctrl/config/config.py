5d3f39a7f0073c3f17521856be47468b6319f45c
William C. Canin <william.costa.canin@gmail.com>
2022-09-07 11:40:01 -0300

    :sparkles: Adding support language

--- snakypy/dotctrl/config/config.py
+++ snakypy/dotctrl/config/config.py
@@ -1,7 +1,6 @@
-# TODO: Adding language option (pt_br ou en_us)
 content = {
     "dotctrl": {
-        "config": {"editor": ""},
+        "config": {"editor": "", "language": "en_US"},
         "elements": [],
     }
 }
