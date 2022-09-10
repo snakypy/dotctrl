1d443df9729af124e6ad73ff8f7cbb7ed4183f3d
William C. Canin <william.costa.canin@gmail.com>
2022-09-10 16:20:30 -0300

    :alien: Adding Portuguese language in the messages

--- snakypy/dotctrl/config/lang.py
+++ snakypy/dotctrl/config/lang.py
@@ -7,6 +7,128 @@ c: Colors = Colors()
 
 LANG: dict = {
     "en_US": {
+        "word:01": "fILES(s)",
+        "word:03": "Element(s)",
+        "word:04": "Directory(s)",
+        "word:05": "Unit(s)",
+        "word:06": "Activated",
+        "word:07": "Disabled",
+        "word:08": "Yes",
+        "word:09": "No",
+        "word:10": "File",
+        "word:11": "Directory",
+        "word:12": "Element",  # empty
+        "word:13": "Total",
+        "word:14": "Folder",
+        "word:15": "Result",
+        "msg:01": "Offered by:",
+        "msg:02": "Repository is empty. No elements.",
+        "msg:03": (
+            f"The elements below are found in the {__info__['name']} directory. "
+            f"""{c.blue('(Type "q" to exit)', c.YELLOW)}\n"""
+        ),
+        "msg:04": f"{c.cyan('[Code:04]', c.YELLOW)} Element not found in repository.",
+        "msg:05": f"{c.cyan('[Code:05]', c.YELLOW)} Unlinked element. Review the same in the repository:",
+        "msg:06": "The repository is already defined in",
+        "msg:07": f"{__info__['name']} is already configured in the directory: ",
+        "msg:08": (
+            "WARNING!\nYou must have SUDO permission on your machine to proceed with this step and create\n"
+            f"an automatic repository with {__info__['name']}.\n"
+            f"You can approach the operation by pressing {c.cyan('Ctrl + C', c.YELLOW)}.\n\n"
+            f"{c.magenta('NOTE', c.YELLOW)}: The directory {__info__['name']} will be created at:"
+        ),
+        "msg:09": "[ Enter the password for sudo ]",
+        "msg:10": f"Initialized {__info__['name']} in the repository: ",
+        "msg:11": (
+            f"{c.cyan('[Code:11]', c.YELLOW)} Element does not exist in {__info__['name']} repository "
+            "to be linked."
+        ),
+        "msg:12": "Element unlinked successfully!",
+        "msg:13": "Type: | Element ",
+        "msg:14": "Nothing to bulk link.",
+        "msg:15": "Element(s) linked successfully!",
+        "msg:16": "Nothing was pulled. Non-existent element.",
+        "msg:17": "Nothing to pull in bulk.",
+        "msg:18": "Element(s) pulled successfully!",
+        "msg:19": "Empty repository. Nothing to call.",
+        "msg:20": "Not linked!",
+        "msg:21": "Congratulations! All elements are linked.",
+        "msg:22": (
+            "The elements below are NOT linked! "
+            f'Use "{c.cyan("dotctrl link", c.YELLOW)}" to link them.'
+        ),
+        "msg:23": f"Repository info {__info__['name']}:",
+        "msg:24": "The repository is empty of registration. No elements.",
+        "msg:25": (
+            f'List of repository element(s) {__info__["name"]}. '
+            f"""{c.blue('(Type "q" to exit)', c.YELLOW)}"""
+        ),
+        "msg:26": "Type:   Element  <-  Symbolic link\n",
+        "msg:27": (
+            f"{c.cyan('[Code:27]', c.YELLOW)} There is a file in the source location. "
+            "If you want to proceed and replace this element found "
+            f"by the {__info__['name']} repository element, use the {c.cyan('--force (--f)', c.YELLOW)} option.\n"
+        ),
+        "msg:28": (
+            "The repository is not created. "
+            f"Use \"{__info__['pkg_name']} init [--auto] [--git]\". Aborted!"
+        ),
+        "msg:29": f"{c.cyan('[Code:29]', c.RED)} Element not found to unlink",
+        "msg:30": "Nothing for bulk unlinked.",
+        "msg:31": "Massively unlinked links successfully!",
+        "msg:32": "Element already linked. Nothing to do.",
+        "msg:33": "",  # empty
+        "msg:34": "",  # empty
+        "msg:35": "Cleaning completed!",
+        "msg:36": "Nothing to clean!",
+        "msg:37": (
+            f"{c.cyan('[Code:37]', c.YELLOW)} Elements with the same name were found in the repository "
+            f"{__info__['name']} and in the source location. These elements are not linked to the repository of the "
+            f"{__info__['name']}.\n"
+            f"To replace those in the {__info__['name']} repository, use the {c.cyan('--force (--f)', c.YELLOW)} option."
+        ),
+        "msg:38": (
+            f"{c.cyan('[Code:38]', c.YELLOW)} It was detected in the {__info__['name']} configuration file that "
+            "some element does not exist in the repository to restore.\n"
+            "This could be caused because the configuration file is out of date.\n"
+            "Before proceeding with the restore, use the command: "
+            f'"{c.cyan("dotctrl config --autoclean", c.YELLOW)}" to update. '
+        ),
+        "msg:39": (
+            f"{c.cyan('[Code:39]', c.YELLOW)} {__info__['name']} encountered a symbolic link to an element that is not "
+            f" linked with the {__info__['name']} repository.\n"
+            "If you want to proceed and replace this symbolic link (and others) found "
+            f"by the {__info__['name']} repository element, use the {c.cyan('--force (--f)', c.YELLOW)} option.\n"
+            f'{c.magenta("Note", c.YELLOW)}: If you use the {c.cyan("--force (--f)", c.YELLOW)} option, this symlink '
+            "found will be removed. "
+            "We recommend that you verify this symbolic link before proceeding with the option "
+            f"{c.cyan('--force (--f)', c.YELLOW)}.\n\n"
+            f"{c.magenta('Symbolic link found')}: "
+        ),
+        "msg:40": "Empty repository. Nothing to restore.",
+        "msg:41": (
+            "This option will bulk restore all elements "
+            f"from the {__info__['name']} repository to the original location.\n   Do you wish to continue?\n"
+        ),
+        "msg:42": "Canceled by user.",
+        "msg:43": f"{c.cyan('[Code:43]', c.RED)} Invalid option!",
+        "msg:44": (
+            f"{c.cyan('[Code:44]', c.YELLOW)} {__info__['name']} FOUND an element at source location with "
+            f"same name as the repository as {__info__['name']}, and this prevented it from proceeding with the restore.\n"
+            f"If you want to replace it with the repository element {__info__['name']}, "
+            f"run this command again with the {c.cyan('--force (--f)', c.YELLOW)} option.\n"
+            f"{c.magenta('Note', c.YELLOW)}: If you use the {c.cyan('--force (--f)', c.YELLOW)} option, this element "
+            "found (and more) will be removed. "
+            "We recommend that you verify this (and other) element(s) before proceeding with the --force (--f) option.\n\n"
+            "Element found: "
+        ),
+        "msg:45": "Operation addressed!",
+        "msg:46": "Complete restoration!",
+        "msg:47": f"Choose a language for the {__info__['name']}:",
+        "msg:48": "Language successfully changed! Changed to:",
+        "msg:49": f"{c.cyan('[Code:49]', c.RED)} Password authentication error.",
+    },
+    "pt_BR": {
         "word:01": "Arquivo(s)",
         "word:03": "Elemento(s)",
         "word:04": "Diretório(s)",
@@ -67,14 +189,14 @@ LANG: dict = {
         "msg:27": (
             f"{c.cyan('[Code:27]', c.YELLOW)} Existe um arquivo no local de origem. "
             "Caso queira prosseguir e substituir este elemento encontrado "
-            f"pelo elemento do repositório {__info__['name']}, use a opção --force (--f).\n"
+            f"pelo elemento do repositório {__info__['name']}, use a opção {c.cyan('--force (--f)', c.YELLOW)}.\n"
         ),
         "msg:28": (
             "O repositório não está criado. "
-            f"Use \"{__info__['pkg_name']} init [--auto] [--git]\". Aborted"
+            f"Use \"{__info__['pkg_name']} init [--auto] [--git]\". Abordado!"
         ),
         "msg:29": f"{c.cyan('[Code:29]', c.RED)} Elemento não encontrado para desvincular",
-        "msg:30": "Nada para desvinculado, em massa.",
+        "msg:30": "Nada para desvinculado em massa.",
         "msg:31": "Links massivamente desvinculados com sucesso!",
         "msg:32": "Elemento já vinculado. Nada a fazer.",
         "msg:33": "",  # empty
@@ -98,7 +220,7 @@ LANG: dict = {
             f"{c.cyan('[Code:39]', c.YELLOW)} O {__info__['name']} encontrou um link simbólico de um elemento que não é "
             f"vinculado com o repositório do {__info__['name']}.\n"
             "Caso queira prosseguir e substituir este link simbólico (e demais) encontrado "
-            f"pelo elemento do repositório {__info__['name']}, use a opção --force (--f).\n"
+            f"pelo elemento do repositório {__info__['name']}, use a opção {c.cyan('--force (--f)', c.YELLOW)}.\n"
             f'{c.magenta("Nota", c.YELLOW)}: Se usar a opção {c.cyan("--force (--f)", c.YELLOW)}, este link simbólico '
             "encontrado será removido. "
             "Recomendamos que, averigue este link simbólico antes de prosseguir com a opção "
